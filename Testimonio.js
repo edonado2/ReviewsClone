2d0636fc2279aa5f8de7a372f06ccaa19fad7538
Ernesto <83133501+edonado2@users.noreply.github.com>
2022-05-01 19:44:57 -0400

    Add files via upload

---
+++ Testimonio.js
@@ -0,0 +1,21 @@
+import React from "react";
+import '../hojas-de-estilo/Testimonios.css'
+
+function Testimonio(props) {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        src={require(`../imagenes/testimonio-${props.imagen}.png`)}
+        alt={props.alt}
+      />
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testtimonio"><strong>{props.nombre}</strong> in {props.pais}</p>
+        <p className="cargo-testimonio">{props.cargo} in <strong>{props.empresa}</strong></p>
+        <p className="texto-testimonio">"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
